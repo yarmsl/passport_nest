93e53d41751e20f29ee04e9eeae63418095543ad
Yaroslav M <38519074+yarmsl@users.noreply.github.com>
2022-04-06 00:28:38 +0500

    swagger fixed (#6)

--- src/auth/dto/tokenRes.credits.ts
+++ src/auth/dto/tokenRes.credits.ts
@@ -1,12 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
-
-export class TokenRes {
+export class AccessTokenRes {
   @ApiProperty({
     example:
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTQsImZpcnN0X25hbWUiOiJKaG9uIiwibGFzdF9uYW1lIjoiV2ljayIsInBob3RvIjpudWxsLCJlbWFpbCI6ImZnZGdkZ2RmZUByci50dHQiLCJwaG9uZSI6IjIxMzQzIiwiYWNjb3VudF90eXBlIjoiZW1haWwiLCJpYXQiOjE2NDkwNDYxNjQsImV4cCI6MTY0OTA0OTc2NH0.DSh-IE7q1Z3WOmm8YIOShRpQbzMZ-IvE7L2uCu3q2OM',
     description: 'Токен доступа',
   })
   access_token: string;
+}
+export class TokensRes extends AccessTokenRes {
   @ApiProperty({
     example:
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTQsImZpcnN0X25hbWUiOiJKaG9uIiwibGFzdF9uYW1lIjoiV2ljayIsInBob3RvIjpudWxsLCJlbWFpbCI6ImZnZGdkZ2RmZUByci50dHQiLCJwaG9uZSI6IjIxMzQzIiwiYWNjb3VudF90eXBlIjoiZW1haWwiLCJpYXQiOjE2NDkwNDYxNjQsImV4cCI6MTY0OTA0OTc2NH0.DSh-IE7q1Z3WOmm8YIOShRpQbzMZ-IvE7L2uCu3q2OM',

f1be2a13526cc03c3cdbad8674253b9977d535e7
yaroskavmsl <slideryo@gmail.com>
2022-04-06 00:28:08 +0500

    swagger fixed

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,14 +3,14 @@ import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { CreateUserDto } from 'src/users/dto/createUser.dto';
 import { AuthService } from './auth.service';
 import { AuthUserDto } from './dto/authUser.dto';
-import { TokenRes } from './dto/tokenRes.credits';
+import { AccessTokenRes, TokensRes } from './dto/tokenRes.credits';
 import { ValidationPipe } from './validation.pipe';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
   @ApiOperation({ summary: 'Регистрация' })
-  @ApiResponse({ status: 200, type: TokenRes })
+  @ApiResponse({ status: 200, type: TokensRes })
   @UsePipes(ValidationPipe)
   @Post('email/signup')
   signup(@Body() signupDto: CreateUserDto) {
@@ -18,7 +18,7 @@ export class AuthController {
   }
 
   @ApiOperation({ summary: 'Авторизация' })
-  @ApiResponse({ status: 200, type: TokenRes })
+  @ApiResponse({ status: 200, type: TokensRes })
   @UsePipes(ValidationPipe)
   @Post('email/signin')
   signin(@Body() signinDto: AuthUserDto) {
@@ -26,7 +26,7 @@ export class AuthController {
   }
 
   @ApiOperation({ summary: 'Проверка авторизации' })
-  @ApiResponse({ status: 200, type: '123' })
+  @ApiResponse({ status: 200, type: TokensRes })
   @UsePipes(ValidationPipe)
   @Get('check')
   check(@Headers('authorization') authHeader: string) {
@@ -34,7 +34,7 @@ export class AuthController {
   }
 
   @ApiOperation({ summary: 'Обновление токена' })
-  @ApiResponse({ status: 200, type: '123' })
+  @ApiResponse({ status: 200, type: AccessTokenRes })
   @UsePipes(ValidationPipe)
   @Get('refresh')
   refresh(@Headers('authorization') authHeader: string) {
